938b79649e2bb44e26f0ebcc26464cd47af0cacd
David Callu <callu.david@gmail.com>
2019-09-17 11:49:27 +0200

    fix(cmake): add CMAKE_DL_LIBS dependencies while cmake not release FindOpenSSL bugfix #3820

--- conan_requirement.cmake
+++ conan_requirement.cmake
@@ -2,3 +2,7 @@
 include("${CMAKE_CURRENT_LIST_DIR}/conan_paths.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/conanbuildinfo.cmake")
 conan_basic_setup()
+
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    link_libraries( ${CMAKE_DL_LIBS} )
+endif()
